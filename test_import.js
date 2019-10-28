91b0367d00f118d980fd565b8575f9cf33a2fde5
bhillier3 <bhillier3@gatech.edu>
2019-10-28 15:24:53 -0400

    can import file, currently does not handle mutiple files or check for specific file types

--- test_import.js
+++ test_import.js
@@ -1,9 +1,24 @@
-var importButton = document.getElementById('importButton');
+var input_image = document.getElementById('select-image');
+var imageContainer = document.getElementById('imgContainer');
 
-var importImage = () => {
-    var imageJS = document.getElementById('image');
-    var image = "pogo_stick.png"
+input_image.addEventListener('change', importImage);
 
-    imageJS.src = image;
-}
-importButton.addEventListener('click', importImage);
+function importImage() {
+    // remove current contents
+    while (imageContainer.childElementCount !== 0) {
+        imageContainer.removeChild(imageContainer.firstChild);
+    }
+
+    // add new (selected) contents
+    var selectedFiles = input_image.files;
+    if (selectedFiles === 0) {
+        let text = document.createElement('P');
+        text.innerHTML = 'No files were selected.';
+        imageContainer.appendChild(text);
+    } else {
+        let image = document.createElement('IMG');
+        // image.src = input_image.value;
+        image.src = window.URL.createObjectURL(selectedFiles[0]);
+        imageContainer.appendChild(image);
+    }
+};
