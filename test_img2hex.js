47449091b5ee741d225c2a5517440bbe54a35d0b
bhillier3 <bhillier3@gatech.edu>
2019-10-29 18:23:38 -0400

    add drawImage function -> draws image on canvas and extracts image pixel data, however am running into crossOrigin error so getImageData() will not run. Looking for solution around this.

--- test_img2hex.js
+++ test_img2hex.js
@@ -10,14 +10,26 @@ function pixel2hex(img_data) {
     return hexcode.toUpperCase();
 };
 
-function draw() {
-    let canvas = document.getElementById('myCanvas');
-    let context = canvas.getContext('2d');
-    let img = document.getElementById('pogo');
-    context.drawImage(img, 0, 0, canvas.width, canvas.height);
-}
+var testObj = document.getElementById('test');
 
-// var testArray = [55, 165, 20, 255];
-// let para = document.createElement('P');
-// para.innerHTML = pixel2hex(testArray);
-// document.getElementById('imgContainer').appendChild(para);
+function drawImage(imageObj) {
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+    var imageX = 0;
+    var imageY = 0;
+    var imageWidth = imageObj.width;
+    var imageHeight = imageObj.height;
+
+    context.drawImage(imageObj, imageX, imageY);
+
+    var imageData = context.getImageData(imageX, imageY, imageWidth, imageHeight);
+    var data = imageData.data;
+
+    testObj.innerHTML = data.toString();
+}
+var imageObj = new Image();
+imageObj.crossOrigin = 'anonymous';
+imageObj.src = './pogo_stick.png';
+imageObj.onload = function() {
+    drawImage(this);
+};
