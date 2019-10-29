a1e028bd26d3a7a0b2f81bbe0ead1ca5b09f81f8
bhillier3 <bhillier3@gatech.edu>
2019-10-29 13:09:38 -0400

    create num2hex function - converts decimal number to hexadecimal number

---
+++ test_img2hex.js
@@ -0,0 +1,7 @@
+function num2hex(num) {
+    // let para = document.createElement('P');
+    // hexstring = num.toString(16);
+    // para.innerHTML = hexstring;
+    // return para;
+    return num.toString(16);
+}
