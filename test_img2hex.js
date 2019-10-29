94d084f65d9a28a551a638a864705ce6c830c44b
bhillier3 <bhillier3@gatech.edu>
2019-10-29 15:50:22 -0400

    add pixel2hex function -> input pixel rgb as array, output color-hexcode representation

--- test_img2hex.js
+++ test_img2hex.js
@@ -1,7 +1,16 @@
-function num2hex(num) {
-    // let para = document.createElement('P');
-    // hexstring = num.toString(16);
-    // para.innerHTML = hexstring;
-    // return para;
+function toHex(num) {
     return num.toString(16);
 }
+
+function pixel2hex(img_data) {
+    let hexcode = '#';
+    for (i = 0; i < 3; i++) {
+        hexcode += toHex(img_data[i]);
+    }
+    return hexcode.toUpperCase();
+}
+
+// var testArray = [55, 165, 20, 255];
+// let para = document.createElement('P');
+// para.innerHTML = pixel2hex(testArray);
+// document.getElementById('imgContainer').appendChild(para);
