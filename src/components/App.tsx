5df1c2dd77a2f67036b10329bb37d33b73b8ae45
Logan Bussell <lbussell3@gatech.edu>
2019-11-15 12:00:15 -0500

    add skeletons for import and export buttons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,22 +3,31 @@ import "../styles/App.css";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ImportButton />
+      <ExportButton />
     </div>
   );
 };
+
+type ButtonProps = {
+  message: string;
+};
+
+const Button = ({ message: message }: ButtonProps) => (
+  <button>{message}</button>
+);
+
+class ImportButton extends React.Component {
+  render() {
+    return <Button message={"Import Image"} />;
+  }
+}
+
+class ExportButton extends React.Component {
+  render() {
+    return <Button message={"Export as .c"} />;
+  }
+}
 
 export default App;
