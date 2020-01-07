2a55a21993ba62b90fd500e651343d4b354749d8
bhillier3 <bhillier3@gatech.edu>
2020-01-07 15:00:20 -0500

    Refactor ExportButton copy

---
+++ src/components/ImportButton copy.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "../styles/buttons.scss";
+
+///////////// Type Definitions:
+type ImportButtonProps = {
+  onImageChange: (imageFile: any) => void;
+};
+
+export default class ImportButton extends React.Component<ImportButtonProps> {
+  fileInput: React.RefObject<HTMLInputElement>;
+  handleImageChange: (file: any) => void;
+  state: {
+    fileName: string;
+    hasLoadedImage: boolean;
+  };
+
+  constructor(props: ImportButtonProps) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.fileInput = React.createRef();
+    this.handleImageChange = props.onImageChange;
+    this.state = {
+      fileName: "No file name",
+      hasLoadedImage: false
+    };
+  }
+
+  handleSubmit(event: React.FormEvent<HTMLInputElement>) {
+    event.preventDefault();
+    if (this.fileInput.current) {
+      if (this.fileInput.current.files) {
+        this.setState({
+          fileName: this.fileInput.current.files[0].name,
+          hasLoadedImage: true
+        });
+        // alert(`Successfully imported ${this.state.fileName}`);
+        this.handleImageChange(this.fileInput.current.files[0]);
+      }
+    }
+  }
+
+  renderLoaded = (imageTitle: string) => (
+    <div className="import-loaded">
+      Loaded <em>{imageTitle}</em>
+    </div>
+  );
+
+  renderUnloaded = () => (
+    <button className="button import-button">
+      <label>
+        Import Image
+        <input
+          type="file"
+          accept=".png, .jpg, .jpeg"
+          ref={this.fileInput}
+          onChange={e => this.handleSubmit(e)}
+        />
+      </label>
+    </button>
+  );
+
+  render() {
+    // return <Button onClick={this.handleClick} message={"Import Image"} />;
+    return this.state.hasLoadedImage
+      ? this.renderLoaded(this.state.fileName)
+      : this.renderUnloaded();
+  }
+}
