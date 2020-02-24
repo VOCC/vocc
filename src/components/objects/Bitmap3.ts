6b7c1c500d135fd2617f58f9caa1e9dcd1424879
Logan Bussell <lbussell3@gatech.edu>
2020-02-24 16:54:41 -0500

    remove redundant code and make image loading always unsafely load an image

--- src/components/objects/Bitmap3.ts
+++ src/components/objects/Bitmap3.ts
@@ -13,38 +13,19 @@ import ImageCanvas from "./ImageCanvas";
 
 export default class Bitmap3 implements ImageInterface {
   public fileName: string;
-  public dimensions: Dimensions = {
-    height: 32,
-    width: 32
-  };
+  public dimensions: Dimensions;
 
-  private hiddenCanvas: HTMLCanvasElement;
-  private hiddenImage: HTMLImageElement;
   private imageData: Uint8ClampedArray;
   private imageCanvas: ImageCanvas;
 
   constructor(
     fileName: string,
-    imageData?: Uint8ClampedArray,
-    dimensions?: Dimensions
+    imageData: Uint8ClampedArray,
+    dimensions: Dimensions
   ) {
     this.fileName = fileName;
-    this.hiddenImage = document.createElement("img");
-    this.hiddenImage.height = this.dimensions.height;
-    this.hiddenImage.width = this.dimensions.width;
-    this.hiddenCanvas = Loader.createHiddenCanvas(this.dimensions);
-
-    if (imageData) {
-      this.imageData = imageData;
-      if (dimensions) {
-        this.dimensions = dimensions;
-      }
-    } else {
-      this.imageData = new Uint8ClampedArray(
-        this.dimensions.height * this.dimensions.width
-      );
-    }
-
+    this.imageData = imageData;
+    this.dimensions = dimensions;
     this.imageCanvas = new ImageCanvas(this);
   }
 
@@ -72,22 +53,12 @@ export default class Bitmap3 implements ImageInterface {
   }
 
   public getPixelColorAt(pos: ImageCoordinates): Color {
-    const context = this.hiddenCanvas.getContext("2d");
-    if (context == null) {
-      return {
-        r: 0,
-        g: 0,
-        b: 0,
-        a: 0
-      };
-    } else {
-      return {
-        r: this.imageData[Loader.offset(pos, this.dimensions)],
-        g: this.imageData[Loader.offset(pos, this.dimensions) + 1],
-        b: this.imageData[Loader.offset(pos, this.dimensions) + 2],
-        a: this.imageData[Loader.offset(pos, this.dimensions) + 3]
-      };
-    }
+    return {
+      r: this.imageData[Loader.offset(pos, this.dimensions)],
+      g: this.imageData[Loader.offset(pos, this.dimensions) + 1],
+      b: this.imageData[Loader.offset(pos, this.dimensions) + 2],
+      a: this.imageData[Loader.offset(pos, this.dimensions) + 3]
+    };
   }
 
   public setPixelColor(
--- src/lib/imageLoadUtils.ts
+++ src/lib/imageLoadUtils.ts
@@ -12,11 +12,9 @@ export const createHiddenCanvas = (d: Dimensions): HTMLCanvasElement => {
 export const loadImageDataFromCanvas = (
   canvas: HTMLCanvasElement,
   dimensions: Dimensions
-): Uint8ClampedArray | void => {
-  const context = canvas.getContext("2d");
-  if (context == null) return;
-  const _data = context.getImageData(0, 0, dimensions.width, dimensions.height);
-  return _data.data;
+): Uint8ClampedArray => {
+  const context = canvas.getContext("2d") as CanvasRenderingContext2D; // unsafe
+  return context.getImageData(0, 0, dimensions.width, dimensions.height).data;
 };
 
 // is async necessary here???? I don't think it is
@@ -42,16 +40,9 @@ export const loadNewImage = async (imageFile: File): Promise<ImageObject> => {
     width: hiddenImage.naturalWidth
   };
   let hiddenCanvas = createHiddenCanvas(dimensions);
-  const context = hiddenCanvas.getContext("2d");
-  if (context != null) {
-    context.drawImage(hiddenImage, 0, 0);
-    let imageData = loadImageDataFromCanvas(hiddenCanvas, dimensions);
-    if (imageData) {
-      return new ImageObject(imageFile.name, imageData, dimensions);
-    }
-  }
-  console.warn("Couldn't load image - loaded blank image instead.");
-  return new ImageObject("img");
+  hiddenCanvas.getContext("2d")?.drawImage(hiddenImage, 0, 0);
+  let imageData = loadImageDataFromCanvas(hiddenCanvas, dimensions);
+  return new ImageObject(imageFile.name, imageData, dimensions);
 };
 
 export const offset = (pos: ImageCoordinates, d: Dimensions) =>
