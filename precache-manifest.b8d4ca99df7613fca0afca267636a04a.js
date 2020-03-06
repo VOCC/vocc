e25e73bb149a07a3792e4902499bc172d5cdec51
Logan Bussell <lbussell3@gatech.edu>
2020-03-05 21:45:50 -0500

    Updates

---
+++ precache-manifest.b8d4ca99df7613fca0afca267636a04a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35474d1855874c291e2b6bf084f9c13",
+    "url": "/vocc/index.html"
+  },
+  {
+    "revision": "7370788af5b11e1dffee",
+    "url": "/vocc/static/css/main.f96f5b1a.chunk.css"
+  },
+  {
+    "revision": "8a7108ed56b5613c831f",
+    "url": "/vocc/static/js/2.751d68af.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/vocc/static/js/2.751d68af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7370788af5b11e1dffee",
+    "url": "/vocc/static/js/main.296009d5.chunk.js"
+  },
+  {
+    "revision": "3a7d9dd72c436e63a567",
+    "url": "/vocc/static/js/runtime-main.b2199a75.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fcc5bf18af151a54e4694c7d5902224f.js"
+  "/vocc/precache-manifest.b8d4ca99df7613fca0afca267636a04a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vocc/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
