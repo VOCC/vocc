d669753a0b11e84091970ba923c29642c25b784e
Logan Bussell <lbussell3@gatech.edu>
2020-03-06 11:19:46 -0500

    Updates

--- precache-manifest.56caedaab56e9a223ff29f938258cbd4.js
+++ precache-manifest.56caedaab56e9a223ff29f938258cbd4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ac8af4ba6e0ead9647501b363b0766ba",
+    "revision": "90e84434cfcbc4cf2b89163992af0a57",
     "url": "/vocc/index.html"
   },
   {
-    "revision": "a638885b1dc5d09a575a",
-    "url": "/vocc/static/css/main.9fc6b429.chunk.css"
+    "revision": "f0345190021cc476b909",
+    "url": "/vocc/static/css/main.a9d48ef9.chunk.css"
   },
   {
     "revision": "70126dc5d7f3fbd4a9b2",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vocc/static/js/2.9f02aef6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a638885b1dc5d09a575a",
-    "url": "/vocc/static/js/main.bb35519a.chunk.js"
+    "revision": "f0345190021cc476b909",
+    "url": "/vocc/static/js/main.5b442e82.chunk.js"
   },
   {
     "revision": "3a7d9dd72c436e63a567",
